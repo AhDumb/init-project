f6b000ce9bedd17d8a18f76cabe4fcd325ccde50
aaron <wzj020109@163.com>
2022-03-02 11:27:35 +0800

    feat(main.cpp): Add print Help and Version cli                 code --help                 code --version

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,20 @@
 #include <iostream>
 #include <string>
 
+/**
+ * 长命令
+ */
 #define LONG_CMD_INIT "init"
-#define SHORT_CMD_INIT "i"
-
 #define LONG_CMD_EDIT "edit"
+#define LONG_CMD_HELP "--help"
+#define LONG_CMD_VERSION "--version"
+/**
+ * 短命令
+ */
+#define SHORT_CMD_INIT "i"
 #define SHORT_CMD_EDIT "e"
+#define SHORT_CMD_HELP "-h"
+#define SHORT_CMD_VERSION "-v"
 
 using namespace std;
 
@@ -39,10 +48,29 @@ void get_out()
   create(name, get_dirs(lang), get_files(lang), get_git(lang));
 }
 
+void print_help()
+{
+  fmt::print("\t\t\t\t初始化项目\n");
+  fmt::print("用法:\n");
+  fmt::print("code \t初始化项目\n");
+  fmt::print("短命令: \n");
+  fmt::print("  code i \t\t初始化项目\n");
+  fmt::print("  code e \t\t编辑配置文件\n");
+  fmt::print("  code -h \t\t查看帮助文档\n");
+  fmt::print("  code -v \t\t查看版本\n");
+  fmt::print("长命令: \n");
+  fmt::print("  code init \t\t初始化项目\n");
+  fmt::print("  code edit \t\t编辑配置文件\n");
+  fmt::print("  code --help \t\t查看帮助文档\n");
+  fmt::print("  code --version \t\t查看版本\n");
+}
+
 int main(int argc, char** argv)
 {
 
-  if (argc == 2) {
+  if (argc == 1) {
+    get_out();
+  } else if (argc == 2) {
     // 把命令参数转成 string 型
     string cmd = argv[1];
 
@@ -53,6 +81,12 @@ int main(int argc, char** argv)
       edit.append(" ");
       edit.append(CONFIG_PATH);
       system(edit.data());
+    } else if (cmd == LONG_CMD_HELP || cmd == SHORT_CMD_HELP) {
+      print_help();
+    } else if (cmd == LONG_CMD_VERSION || cmd == SHORT_CMD_VERSION) {
+      fmt::print(
+          fg(fmt::color::light_green),
+          "版本: {:.1f}\n", 1.0);
     } else {
       fmt::print(
           fg(fmt::color::crimson) | fmt::emphasis::bold,
